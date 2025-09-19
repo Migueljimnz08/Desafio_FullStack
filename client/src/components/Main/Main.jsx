0e8b461506be09bd73f589d0ce4e1a56a428cbf2
Miguel <migueljm1008@gmail.com>
2025-09-19 18:37:17 +0200

    add/NotFound and Login views aswell as userServices

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './NotFound';
+import Login from "./Login/Login";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main className="main">
+      <Routes>
+        <Route path='/' element={< />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/*' element={<NotFound/>} />
+      </Routes>
+    </main>
+  );
 };
 
 export default Main;
