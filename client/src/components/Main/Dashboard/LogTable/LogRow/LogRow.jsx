26cca76a49c1774759e1d8af370813488e0bee69
NazaretMelquiades <nazaretmelquiades@hotmail.com>
2025-09-24 13:49:03 +0200

    add/style with MaterialUI from LogTable

--- client/src/components/Main/Dashboard/LogTable/LogRow/LogRow.jsx
+++ client/src/components/Main/Dashboard/LogTable/LogRow/LogRow.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
 
-const LogRow = ({ log }) => {
-  const [showMore, setShowMore] = useState(false);
+// const LogRow = ({ log }) => {
+//   const [showMore, setShowMore] = useState(false);
 
-  // const getSeverityClass = () => {
-  //   if (log.severity >= 8) return "high-severity";
-  //   if (log.severity >= 5) return "medium-severity";
-  //   return "low-severity";
-  // };
+//   // const getSeverityClass = () => {
+//   //   if (log.severity >= 8) return "high-severity";
+//   //   if (log.severity >= 5) return "medium-severity";
+//   //   return "low-severity";
+//   // };
 
-  return (
-    <tr>
-      <td>{log.id}</td>
-      <td>{log.company_id}</td>
-      <td>{log.status}</td>
-      <td>{log.type}</td>
-      <td>{log.indicators}</td>
-      <td>{log.severity}</td>
-      <td>{log.date}</td>
-      <td>{log.time}</td>
-      <td>
-        <button
-          onClick={() => setShowMore(!showMore)}
-          style={{ marginLeft: "5px" }}
-        >
-          {showMore ? "Mostrar menos" : "Mostrar más"}
-        </button>
-      </td>
-    </tr>
-  );
-};
+//   return (
+//     <tr>
+//       <td>{log.id}</td>
+//       <td>{log.company_id}</td>
+//       <td>{log.status}</td>
+//       <td>{log.type}</td>
+//       <td>{log.indicators}</td>
+//       <td>{log.severity}</td>
+//       <td>{log.date}</td>
+//       <td>{log.time}</td>
+//       <td>
+//         <button
+//           onClick={() => setShowMore(!showMore)}
+//           style={{ marginLeft: "5px" }}
+//         >
+//           {showMore ? "Mostrar menos" : "Mostrar más"}
+//         </button>
+//       </td>
+//     </tr>
+//   );
+// };
 
 
-export default LogRow;
+// export default LogRow;
