c2e3293f98d43d2b7083a5c6db40bf2b153757ad
Miguel <migueljm1008@gmail.com>
2025-09-25 13:47:14 +0200

    add/Graphs related to the data that the logs have

--- client/src/components/Main/Dashboard/Dashboard.jsx
+++ client/src/components/Main/Dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import LogsTable from "./LogTable/LogTable";
+import Graphs from "./Graphs/Graphs";
 
 const Dashboard = () => {
   return <section>
-    
+    <Graphs />
     <LogsTable />
   </section>;
 };
--- client/src/components/Main/Dashboard/Graphs/Graphs.jsx
+++ client/src/components/Main/Dashboard/Graphs/Graphs.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
+import { getDdosGraph, getPhishingGraph, getLoginGraph } from "../../../../services/graphServices";
+import { ThreeDots } from 'react-loader-spinner';
 
-const Graphs = () => {
-  return <div>Graphs</div>;
-};
+function App() {
+  const [ddosGraph, setDdosGraph] = useState(null);
+  const [phishingGraph, setPhishingGraph] = useState(null);
+  const [loginGraph, setLoginGraph] = useState(null);
 
-export default Graphs;
+  useEffect(() => {
+    const getGraphs = async () => {
+      const ddos = await getDdosGraph();
+      console.log(ddos);
+      setDdosGraph(ddos)
+      const phishing = await getPhishingGraph();
+      console.log(phishing);
+      setPhishingGraph(phishing)
+      const login = await getLoginGraph();
+      console.log(login);
+      setLoginGraph(login)
+    };
+    getGraphs()
+  }, []);
+
+  return (
+    <article>
+      <h1>Gráficas de las alertas</h1>
+      {ddosGraph && phishingGraph && loginGraph ? ( 
+        <>
+        <Plot data={ddosGraph.data} layout={ddosGraph.layout} />
+        <Plot data={phishingGraph.data} layout={phishingGraph.layout} />
+        <Plot data={loginGraph.data} layout={loginGraph.layout} /> 
+        </>
+      ) : (
+        <>
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#007bff"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          />
+        <p>Cargando gráficas...</p>
+        </>
+      )}
+    </article>
+  );
+}
+export default App;
