76fb86882caa7f8c7af3556acbfaccff0b5c9d82
Miguel <migueljm1008@gmail.com>
2025-09-25 18:08:37 +0200

    add/visible graphs and styles to dashboard

--- client/src/components/Main/Dashboard/Graphs/Graphs.jsx
+++ client/src/components/Main/Dashboard/Graphs/Graphs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Plot from "react-plotly.js";
-import { getDdosGraph, getPhishingGraph, getLoginGraph } from "../../../../services/graphServices";
+import { getDdosGraph, getPhishingGraph, getLoginGraph, ddosLayout, phishingLayout, loginLayout } from "../../../../services/graphServices";
 import { ThreeDots } from 'react-loader-spinner';
 
 function App() {
@@ -11,27 +11,23 @@ function App() {
   useEffect(() => {
     const getGraphs = async () => {
       const ddos = await getDdosGraph();
-      console.log(ddos);
       setDdosGraph(ddos)
       const phishing = await getPhishingGraph();
-      console.log(phishing);
       setPhishingGraph(phishing)
       const login = await getLoginGraph();
-      console.log(login);
       setLoginGraph(login)
     };
     getGraphs()
   }, []);
 
   return (
-    <article>
-      <h1>Gráficas de las alertas</h1>
+    <article className="graphs">
       {ddosGraph && phishingGraph && loginGraph ? ( 
         <>
-        <Plot data={ddosGraph.data} layout={ddosGraph.layout} />
-        <Plot data={phishingGraph.data} layout={phishingGraph.layout} />
-        <Plot data={loginGraph.data} layout={loginGraph.layout} /> 
-        </>
+        <Plot data={ddosGraph.data} layout={ddosLayout} config={{ displayModeBar: false }}/>
+        <Plot data={phishingGraph.data} layout={phishingLayout} config={{ displayModeBar: false }}/>
+        <Plot data={loginGraph.data} layout={loginLayout} config={{ displayModeBar: false }}/> 
+        </> 
       ) : (
         <>
         <ThreeDots
