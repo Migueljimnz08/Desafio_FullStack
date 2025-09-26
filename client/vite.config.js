88692d7bc225229f43b2585b446edc02b5d62470
NazaretMelquiades <nazaretmelquiades@hotmail.com>
2025-09-26 10:09:46 +0200

    deleted compression

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    compression({ algorithm: 'gzip' }),
-  ],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
